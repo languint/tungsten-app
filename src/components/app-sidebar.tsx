54aff97e514f6e08435d88902417e74f816095a8
languint <longuint@proton.me>
2025-11-06 12:02:46 -0600

    refactor: sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,8 +1,8 @@
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarSeparator } from "./ui/sidebar";
 import { TooltipProvider } from "./ui/tooltip";
-import { useDataManager } from "@/hooks/data-manager";
+import { DataManager, useDataManager } from "@/hooks/data-manager";
 import { convertFileSrc } from "@tauri-apps/api/core";
-import { usePlaylists } from "@/hooks/playlists";
+import { Playlist, Playlists, usePlaylists } from "@/hooks/playlists";
 import * as path from '@tauri-apps/api/path';
 import { useEffect, useState } from "react";
 import { ListMusicIcon } from "lucide-react";
@@ -13,7 +13,6 @@ export function AppSidebar() {
     const [localDataDir, setLocalDataDir] = useState<string | null>(null);
 
     useEffect(() => {
-
         const getLocalDataDir = async () => {
             const home = await path.localDataDir();
             setLocalDataDir(home);
@@ -27,32 +26,37 @@ export function AppSidebar() {
     return (
         <Sidebar className="h-[calc(100%-4rem)]">
             <SidebarHeader></SidebarHeader>
-            <SidebarContent>
-                <SidebarSeparator />
-                <SidebarGroup>
-                    <SidebarGroupContent >
-                        <TooltipProvider>
-                            {playlists.map((playlist) => (
-                                <button className="w-full border-none px-2 flex flex-row gap-2">
-                                    <div className="grow max-w-15 aspect-square border rounded-md bg-muted flex items-center justify-center">
-                                        {playlist.cover ? <img
-                                            className="rounded-md grow max-w-15 aspect-square border"
-                                            src={convertFileSrc(
-                                                `${dataManager?.dataDir ?? `${localDataDir}/tungsten_data`}/covers/${playlist.cover}`
-                                            )}
-                                        />
-                                            : <ListMusicIcon className="stroke-muted-foreground w-15" />}
-                                    </div>
-                                    <div className="grow flex flex-col items-start">
-                                        <p>{playlist.name}</p>
-                                        <p className="text-muted-foreground">Playlist - {playlist.songs.length} track(s)</p>
-                                    </div>
-                                </button>
-                            ))}
-                        </TooltipProvider>
-                    </SidebarGroupContent>
-                </SidebarGroup>
+            <SidebarContent >
+                {AppSidebarPlaylists(playlists, dataManager, localDataDir)}
             </SidebarContent>
         </Sidebar>
     )
 }
+
+function AppSidebarPlaylists(playlists: Playlists, dataManager: DataManager | undefined, localDataDir: string | null) {
+    return <SidebarGroup>
+        <SidebarGroupContent>
+            {playlists.map((playlist) => (
+                <button className="w-full flex flex-row gap-2 px-2">
+                    {AppSidebarPlaylistCover(playlist, dataManager, localDataDir)}
+                    <div className="grow flex flex-col items-start">
+                        <p>{playlist.name}</p>
+                        <p className="text-muted-foreground">Playlist - {playlist.songs.length} track(s)</p>
+                    </div>
+                </button>
+            ))}
+        </SidebarGroupContent>
+    </SidebarGroup>;
+}
+
+function AppSidebarPlaylistCover(playlist: Playlist, dataManager: DataManager | undefined, localDataDir: string | null) {
+    return <div className="grow max-w-15 aspect-square border rounded-md bg-muted flex items-center justify-center">
+        {playlist.cover ? <img
+            className="rounded-md grow max-w-15 aspect-square border"
+            src={convertFileSrc(
+                `${dataManager?.dataDir ?? `${localDataDir}/tungsten_data`}/covers/${playlist.cover}`
+            )} />
+            : <ListMusicIcon className="stroke-muted-foreground w-15" />}
+    </div>;
+}
+
